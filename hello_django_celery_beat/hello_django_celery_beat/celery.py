7e0ef47ff750b4ca1815f9b3901c7dac4fe27452
zkan <kan@prontomarketing.com>
2019-01-15 20:09:03 +0700

    Fix module name :bear:

--- hello_django_celery_beat/hello_django_celery_beat/celery.py
+++ hello_django_celery_beat/hello_django_celery_beat/celery.py
@@ -3,7 +3,7 @@ import os
 from celery import Celery
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_celery.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hello_django_celery_beat.settings')
 
 app = Celery('my_django_celery_beat')
 app.conf.broker_url = 'redis://localhost:6379/0'
